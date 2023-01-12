970ac6c889dd29a27f78f784c2da3ac8e3c08b7c
Daniel Hramkov <daniel.hramkov@gmail.com>
2023-01-12 07:35:13 +0100

    Create blend to file usage like source

--- Assets/Editor/SyncBridge/MeshToPrefabs.cs
+++ Assets/Editor/SyncBridge/MeshToPrefabs.cs
@@ -11,8 +11,18 @@ using UnityEditor.SceneManagement;
 public class MeshToPrefabs : MonoBehaviour
 {
 
+    static string folderPath;
 
-    [MenuItem("SyncBridge/Sync It")]
+    static string sourceFolder;
+    static string meshesFolder;
+    static string prefabsFolder;
+    static string materialsFolder;
+
+    static string sceneName;
+    static string sceneMeshPath;
+
+
+[MenuItem("SyncBridge/Sync It")]
     static void Main()
     {
         
@@ -50,22 +60,50 @@ public class MeshToPrefabs : MonoBehaviour
     [MenuItem("Assets/Sync Folder")]
     static void SyncFolder()
     {
+
+
+        if (!SetUpFolders())
+        {
+            return;
+        }
+
+
+        if (!GenerateScenePrefabs())
+        {
+            return;
+        }
+
+    }
+
+
+    static bool SetUpFolders()
+    {
+
+
         // Get path then you make asign
-        string folderPath = GetPath();
+        folderPath = GetPath();
+        // Set folders
+        meshesFolder = folderPath + "/meshes/";
+        prefabsFolder = folderPath + "/prefabs/";
+        materialsFolder = folderPath + "/materials/";
 
-        string meshesFolder = folderPath + "/meshes";
-        string prefabsFolder = folderPath + "/prefabs";
-        string materialsFolder = folderPath + "/materials";
+        sourceFolder = folderPath + "/_source/";
 
-        string sceneName = folderPath.Substring(folderPath.LastIndexOf("/"));
-        string sceneMeshPath = meshesFolder + sceneName + ".fbx";
+        if (!AssetDatabase.IsValidFolder(sourceFolder))
+        {
+            Debug.LogError("Nothing to sync: make sure you have blend file into _source folder!");
+            return false;
+        }
 
-        // Create /meshes and /prefabs and /materials folders if not exist
+        sceneName = folderPath.Substring(folderPath.LastIndexOf("/"));
+        sceneMeshPath = meshesFolder + sourceFolder + sceneName + ".blend";
+
+        // Create folders if not exist
         if (!AssetDatabase.IsValidFolder(meshesFolder))
         {
             AssetDatabase.CreateFolder(folderPath, "meshes");
         }
-        
+
         if (!AssetDatabase.IsValidFolder(prefabsFolder))
         {
             AssetDatabase.CreateFolder(folderPath, "prefabs");
@@ -76,65 +114,111 @@ public class MeshToPrefabs : MonoBehaviour
             AssetDatabase.CreateFolder(folderPath, "materials");
         }
 
-        string meshFolderPath = folderPath + "/meshes";
-        string prefabFolderPath = folderPath + "/prefabs";
-
-        // Get all mesh assets in the mesh folder
-        string[] meshAssetPaths = AssetDatabase.FindAssets("t:Mesh", new[] { meshFolderPath });
-
-        foreach (string meshAssetPath in meshAssetPaths)
-        {
-
-            GameObject asset = AssetDatabase.LoadAssetAtPath<GameObject>(AssetDatabase.GUIDToAssetPath(meshAssetPath));
-
-            string assetName = asset.name;
-
-            // Check if the mesh name includes the string "PREFABS"
-            if (assetName.Contains("_PREFAB"))
-            {
-                // Create a new game object and add the mesh as a component
-                GameObject gameObject = new GameObject(assetName.Replace("_PREFAB", ""));
-                MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
-                MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
-                meshFilter.sharedMesh = asset.GetComponent<MeshFilter>().sharedMesh;
-
-
-                // Extract and assign material
-
-                Material[] materials = asset.GetComponent<MeshRenderer>().sharedMaterials;
-                Material[] newMaterials = new Material[materials.Length];
-                 
-                for (int i = 0; i < materials.Length; i++)
-                {
-                    string matPath = folderPath + "/materials/" + materials[i].name + ".mat";
-                    Material temp = Instantiate(materials[i]);
-                    AssetDatabase.CreateAsset(temp, matPath);
-                    newMaterials[i] = AssetDatabase.LoadAssetAtPath<Material>(matPath);
-
-                }
-                 
-                meshRenderer.sharedMaterials = newMaterials;
-
-                // Create a prefab asset for the game object
-                string prefabPath = prefabFolderPath + "/" + assetName.Replace("_PREFAB", "") + ".prefab";
-                PrefabUtility.SaveAsPrefabAsset(gameObject, prefabPath);
-
-                // Destroy the temporary game object
-                DestroyImmediate(gameObject);
-            }
-        }
-
-        GenerateSceneByMesh(sceneMeshPath, prefabsFolder, folderPath + sceneName + ".unity");
-
+        return true;
     }
 
-    static void GenerateSceneByMesh(string meshPath, string prefabsPath, string scenePath)
+    static bool GenerateScenePrefabs()
+    {
+        // Get all mesh assets in the mesh folder
+        string[] guid = AssetDatabase.FindAssets("t:Mesh", new[] { sourceFolder });
+
+        GameObject asset = AssetDatabase.LoadAssetAtPath<GameObject>(AssetDatabase.GUIDToAssetPath(guid[0]));
+
+        Mesh test = asset.GetComponent<Mesh>();
+
+        int prefabCount = 0;
+
+        //foreach (string meshGUIDS in meshesGUIDSFromSource)
+        //{
+        //
+        //    
+        //
+        //    
+        //    string assetName = asset.name;
+        //
+        //    Scene newScene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
+        //
+        //    // Check if the mesh name includes the "."
+        //    if (!assetName.Contains("."))
+        //    {
+        //        // Generate Prefab
+        //
+        //        prefabCount += 1;
+        //
+        //        // Create a new game object and add the mesh as a component
+        //        GameObject gameObject = new GameObject(assetName);
+        //        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+        //        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        //        meshFilter.sharedMesh = asset.GetComponent<MeshFilter>().sharedMesh;
+        //
+        //
+        //        // Extract and assign material
+        //
+        //        Material[] materials = asset.GetComponent<MeshRenderer>().sharedMaterials;
+        //        Material[] newMaterials = new Material[materials.Length];
+        //
+        //        for (int i = 0; i < materials.Length; i++)
+        //        {
+        //            string matPath = materialsFolder + materials[i].name + ".mat";
+        //            Material temp = Instantiate(materials[i]);
+        //            AssetDatabase.CreateAsset(temp, matPath);
+        //            newMaterials[i] = AssetDatabase.LoadAssetAtPath<Material>(matPath);
+        //
+        //        }
+        //
+        //        meshRenderer.sharedMaterials = newMaterials;
+        //
+        //        // Create a prefab asset for the game object
+        //        string prefabPath = prefabsFolder + assetName + ".prefab";
+        //        PrefabUtility.SaveAsPrefabAsset(gameObject, prefabPath);
+        //
+        //        // Destroy the temporary game object
+        //        DestroyImmediate(gameObject);
+        //
+        //    } else
+        //    {
+        //        // Generate scene mesh
+        //
+        //        // Delete all after dot in name
+        //        string prefabName = asset.transform.name;
+        //        int index = prefabName.IndexOf(".");
+        //        prefabName = prefabName.Substring(0, index);
+        //
+        //
+        //        // Get the prefab asset with the same name as the child transform
+        //        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabsFolder + prefabName + ".prefab");
+        //
+        //        // Instantiate the prefab in the scene
+        //        GameObject instance = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+        //
+        //        // Set the position and rotation of the instance to match the child transform
+        //        instance.transform.position = asset.transform.position;
+        //        instance.transform.rotation = asset.transform.rotation;
+        //
+        //
+        //        // Save the scene
+        //        EditorSceneManager.SaveScene(newScene, folderPath + sceneName + ".unity");
+        //    }
+        //}
+
+        if(prefabCount == 0)
+        {
+            Debug.LogError("Source blend file has no any prefabs: make sure your prefabs ain't contains '.' symbol.");
+            return false;
+        }
+
+        return true;
+    }
+
+
+    static bool GenerateSceneByMesh()
     {
         // Get scene fbx
-        GameObject sceneMesh = AssetDatabase.LoadAssetAtPath<GameObject>(meshPath);
+        GameObject sceneMesh = AssetDatabase.LoadAssetAtPath<GameObject>(sceneMeshPath);
         Transform meshTransform = sceneMesh.transform;
 
         Scene newScene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
+
 
         for (int i = 0; i < meshTransform.childCount; i++)
         {
@@ -148,7 +232,7 @@ public class MeshToPrefabs : MonoBehaviour
                 
 
             // Get the prefab asset with the same name as the child transform
-            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabsPath + "/" + prefabName + ".prefab");
+            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabsFolder + prefabName + ".prefab");
 
             // Instantiate the prefab in the scene
             GameObject instance = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
@@ -158,8 +242,7 @@ public class MeshToPrefabs : MonoBehaviour
             instance.transform.rotation = childTransform.rotation;
         }
 
-        // Save the scene
-        EditorSceneManager.SaveScene(newScene, scenePath);
+        return true;
     }
 
 
